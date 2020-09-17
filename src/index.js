bae0327c2682b4453fe2508ec98cde94c0f05795
Kenji <kenji.au9@gmail.com>
2020-09-17 11:11:32 -0600

    Added in HelloStudent component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HelloStudent from './components/HelloStudent';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <></>
+    <h1>My Git Test.</h1>
+    <HelloStudent name="Kenji"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
