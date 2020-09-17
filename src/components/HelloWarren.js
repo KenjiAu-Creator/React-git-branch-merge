356b44aa3c6685f3c86e87ee0bdf233994e15736
Kenji <kenji.au9@gmail.com>
2020-09-17 12:56:38 -0600

    Added HelloWarren compnonent

---
+++ src/components/HelloWarren.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HelloStudent from './HelloStudent';
+
+function HelloWarren() {
+  return (
+    <p>Hi there, Warren!</p>
+  );
+};
+
+export default HelloWarren;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import HelloWorld from './components/HelloWorld';
 import HelloStudent from './components/HelloStudent';
 import './index.css';
+import HelloWarren from './components/HelloWarren';
 
 ReactDOM.render(
   <React.StrictMode>
     <h1>React Git Branch Merge Practice</h1>
     <HelloWorld />
     <HelloStudent name="Kenji"/>
+    <HelloWarren />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
